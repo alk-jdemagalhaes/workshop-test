0f3bbd3c0ff0fcce729c38feae6967daa999fb28
jde-maga <d.julien78+github@gmail.com>
2021-11-25 10:08:14 +0100

    Add ex01

---
+++ src/exercises/ex01/index.tsx
@@ -0,0 +1,14 @@
+const Ex01 = () => {
+  return (
+    <div>
+      <span>Hello World !</span>
+      {false && <span>This is not showing up !</span>}
+      <button>My bouton</button>
+      {false && <a>My link</a>}
+      <div data-testid="test1">Test1</div>
+      {false && <div data-testid="test2">Test2</div>}
+    </div>
+  );
+};
+
+export default Ex01;
